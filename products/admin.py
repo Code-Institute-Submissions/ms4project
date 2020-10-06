6afdda056426c0ecd5492f6c37e3557c25ade741
Andrew Ralston <andrewralston35@gmail.com>
2020-10-06 10:15:42 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 from .models import Beer, Style, Brewery
 
-admin.site.register(Beer)
-admin.site.register(Brewery)
-admin.site.register(Style)
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'style',
+        'price',
+        'rating',
+        'volume',
+        'abv',
+        'image',
+        'brewery',
+    )
+
+    ordering = ('sku',)
+
+
+class StyleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class BreweryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'country'
+    )
+
+
+admin.site.register(Beer, BeerAdmin)
+admin.site.register(Style, StyleAdmin)
+admin.site.register(Brewery, BreweryAdmin)
+
--- products/models.py
+++ products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Style(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,8 +13,12 @@ class Style(models.Model):
 
 
 class Brewery(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Breweries'
+        
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, blank=True)
     country = models.CharField(max_length=254)
 
     def __str__(self):
@@ -27,16 +32,21 @@ class Brewery(models.Model):
 
 
 class Beer(models.Model):
-    style = models.ForeignKey('Style', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    style = models.ForeignKey('Style', null=True,
+                              blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    volume = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
-    abv = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    brewery = models.ForeignKey('Brewery', null=True, blank=True, on_delete=models.SET_NULL)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
+    volume = models.DecimalField(max_digits=6, decimal_places=1,
+                                 null=True, blank=True)
+    abv = models.DecimalField(max_digits=6, decimal_places=1,
+                              null=True, blank=True)
+    image = models.ImageField(blank=True)
+    brewery = models.ForeignKey('Brewery', null=True,
+                                blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
