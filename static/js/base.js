7a9af38402c29a9dff9940d1de62f0c68a1cd5ca
Andrew Ralston <andrewralston35@gmail.com>
2020-10-07 02:34:17 +0000

    added style, brewery and sort by to beers page

--- static/js/base.js
+++ static/js/base.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('select').formSelect();
   });
