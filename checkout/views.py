376a3d9cc2abb59638ea0806745f6a014921e74a
Andrew Ralston <andrewralston35@gmail.com>
2020-10-11 02:27:10 +0000

    added checkout template with payment form

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import Orderform
 
-# Create your views here.
+
+def checkout(request):
+    cart = request.session.get('bag', {})
+    if not cart:
+        messages.error(request,  "Your cart is empty")
+        return redirect(reverse('beers'))
+
+    order_form = Orderform()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- hits_n_hops/settings.py
+++ hits_n_hops/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'material'
 ]
 
 MIDDLEWARE = [
