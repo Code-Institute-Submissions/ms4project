f00f70b8e8c9d5a6879e6fec8f6af871c5402450
Andrew Ralston <andrewralston35@gmail.com>
2020-10-20 08:00:00 +0000

    updated webhooks and checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,7 +17,7 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    '''def _send_confirmation_email(self, order):
+    def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
@@ -32,23 +32,122 @@ class StripeWH_Handler:
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )'''
+        )
 
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'unhandled Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the "payment_intent.succeeded" webhook from Stripe
         """
+
+        # Get the payment intent and metadata
+        intent = event.data.object
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # Clean/strip form data and set empty values to None
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != "AnonymousUser":
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.\
+                    line1
+                profile.default_street_address2 = shipping_details.address.\
+                    line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
+        # Try to get the order in the database using the form data
+        attempt = 1
+        order_exists = False
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
+                order_exists = True  # Nothing else to do if order is already created
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)  # Sleep for a second and then try again
+        if order_exists:
+            # Send a confirmation email
+            self._send_confirmation_email(order)
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            # If somehow the form didn't create the order, try to create it here
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
+                for item_id, quantity in json.loads(cart).items():
+                    beer = Beer.objects.get(id=item_id)
+                    if isinstance(quantity, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            beer=beer,
+                            quantity=quantity
+                        )
+                        order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                    # Delete if anything wrong Stripe tries again
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        # Send a confirmation email
+        self._send_confirmation_email(order)
         return HttpResponse(
                 content=f'Webhook received: {event["type"]} |\
-                     SUCCESS: Created order in webhook',
+                    SUCCESS: Created order in webhook',
                 status=200)
 
     def handle_payment_intent_payment_failed(self, event):
