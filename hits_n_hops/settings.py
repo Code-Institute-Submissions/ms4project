89cc2ee0e4eca8cbf5026c81879c5f8f84379ce3
Andrew Ralston <andrewralston35@gmail.com>
2020-11-15 19:48:59 +0000

    removed secret key from settings.py

--- hits_n_hops/settings.py
+++ hits_n_hops/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!b&40l8t-8t!9pv_gs$!*90q3+gayqc2et2gvf^vd(u!%wb$$#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hitsnhops.herokuapp.com', 'localhost']
 
